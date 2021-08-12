777fa69777889268a157fe76397cfd9b4b93b075
FaKenKoala <60820399+FaKenKoala@users.noreply.github.com>
2021-08-12 15:31:27 +0800

    5.0.0

--- CustomButton/SexPicStyleButton.m
+++ CustomButton/SexPicStyleButton.m
@@ -13,8 +13,8 @@
 
 -(void)layoutSubviews{
     [super layoutSubviews];
-    self.imageView.frame=CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.bounds.size.width/3.0, self.bounds.size.height);
-    self.titleLabel.frame=CGRectMake(self.bounds.origin.x+self.bounds.size.width/3.0-2, self.bounds.origin.y, self.bounds.size.width/3.0*2, self.bounds.size.height);
+    self.imageView.frame=CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.bounds.size.width/5.0, self.bounds.size.height);
+    self.titleLabel.frame=CGRectMake(self.bounds.origin.x+self.bounds.size.width/5.0-2, self.bounds.origin.y, self.bounds.size.width/5.0*2, self.bounds.size.height);
     self.titleLabel.textAlignment=NSTextAlignmentRight;
 }
 
